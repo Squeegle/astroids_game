f4a1293219c08e290ed2476ec23a5ef499249724
Squeegle <remi.gould@gmail.com>
2025-04-14 00:08:34 -0300

    completed L5

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
 
     print("Starting Astroids")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -18,9 +24,12 @@ def main():
                 return
 
         screen.fill((0, 0, 0))
+        player = Player(x, y)
+        player.draw(screen)
         pygame.display.flip()
 
-    pygame.quit()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
